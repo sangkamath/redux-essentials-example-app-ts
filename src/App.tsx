9e93440135c8d51d812d830f2d45224de165aecc
sangkamath <twinkle_kamath@yahoo.com>
2025-02-10 16:11:06 -0800

    Step 5: Add SinglePostPage

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 
 import { Navbar } from './components/Navbar'
-import { PostsList } from './features/posts/PostsList'
-import { AddPostForm } from './features/posts/AddPostForm'
+import { PostsMainPage } from './features/posts/PostsMainPage'
+import { SinglePostPage } from './features/posts/SinglePostPage'
 
 function App() {
   return (
@@ -12,13 +12,13 @@ function App() {
         <Routes>
           <Route
             path="/"
-            element={
-              <>
-              <AddPostForm />
-              <PostsList />
-              </>
-            }
+            element={<PostsMainPage />}
           ></Route>
+          <Route 
+            path="/posts/:postId" 
+            element={<SinglePostPage />}
+          >
+          </Route>
         </Routes>
       </div>
     </Router>
