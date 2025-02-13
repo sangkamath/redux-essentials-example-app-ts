2e26daafba5c938a2faecd02fa3cd1e567b8115c
sangkamath <twinkle_kamath@yahoo.com>
2025-02-12 18:28:37 -0800

    Step 17: Add a LoginPage and require login to view posts

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,24 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom'
 
 import { Navbar } from './components/Navbar'
 import { PostsMainPage } from './features/posts/PostsMainPage'
 import { SinglePostPage } from './features/posts/SinglePostPage'
 import { EditPostForm } from './features/posts/EditPostForm'
+import { useAppSelector } from './app/hook'
+import { LoginPage } from './features/auth/LoginPage'
+
+import { selectCurrentUsername } from './features/auth/authSlice'
+
+const ProtectedRoute = ({ children }: { children: React.ReactNode }) => {
+  const username = useAppSelector(selectCurrentUsername)
+  console.log(username)
+
+  if (!username) {
+    return <Navigate to="/" replace />
+  }
+
+  return children
+}
 
 function App() {
   return (
@@ -11,18 +26,25 @@ function App() {
       <Navbar />
       <div className="App">
         <Routes>
-          <Route
-            path="/"
-            element={<PostsMainPage />}
-          ></Route>
-          <Route 
-            path="/posts/:postId" 
-            element={<SinglePostPage />}
-          />
-          <Route
-            path="/editPost/:postId"
-            element={<EditPostForm />}
-          />
+          <Route path="/" element={<LoginPage />} />
+          <Route path="/*" element={
+            <ProtectedRoute>
+              <Routes>
+                <Route
+                  path="/posts"
+                  element={<PostsMainPage />}
+                ></Route>
+                <Route
+                  path="/posts/:postId"
+                  element={<SinglePostPage />}
+                />
+                <Route
+                  path="/editPost/:postId"
+                  element={<EditPostForm />}
+                />
+              </Routes>
+            </ProtectedRoute>
+          } />
         </Routes>
       </div>
     </Router>
